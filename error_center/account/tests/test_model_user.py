7ea6f4633ec5f8cd9ab24f7d75f4ef621b8a31a1
Carlos Moreno <omorenodovale@gmail.com>
2020-07-05 20:23:45 -0500

    Add docstring in file of tests

--- error_center/account/tests/test_model_user.py
+++ error_center/account/tests/test_model_user.py
@@ -20,9 +20,11 @@ class UserModelTest(TestCase):
         )
 
     def test_create_user(self):
+        """"Must return True if exist users"""
         self.assertTrue(User.objects.exists())
 
     def test_email_error(self):
+        """Must validate if email is valid"""
         user = User(
             first_name="Ciclano",
             last_name="de Tal",
@@ -32,23 +34,29 @@ class UserModelTest(TestCase):
         self.assertRaises(ValidationError, user.full_clean)
 
     def test_user_is_staff(self):
+        """Must return True if user is staff"""
         self.assertTrue(self.user_admin.is_staff)
 
     def test_user_is_not_staff(self):
+        """Must return False if user not is staff"""
         self.assertFalse(self.user_common.is_staff)
 
     def test_full_name(self):
+        """Must return full name of user"""
         self.assertEqual(self.user_common.get_full_name(), "Fulano de Tal")
         self.assertEqual(self.user_admin.get_full_name(), "Beltrano de Tal")
 
     def test_short_name(self):
-        self.assertEqual(self.user_common.get_short_name(), "Fulano de")
-        self.assertEqual(self.user_admin.get_short_name(), "Beltrano de")
+        """Must return short name of user"""
+        self.assertEqual(self.user_common.get_short_name(), "Fulano Tal")
+        self.assertEqual(self.user_admin.get_short_name(), "Beltrano Tal")
 
     def test_str(self):
+        """Checks if the __str__ method is the same as the user's full name"""
         self.assertEqual(str(self.user_common), "Fulano de Tal")
         self.assertEqual(str(self.user_admin), "Beltrano de Tal")
 
     def test_get_user_by_email(self):
+        """Must return user by email"""
         u = User.objects.get(email="fulano@xpto.com")
         self.assertTrue(self.user_common, u)
