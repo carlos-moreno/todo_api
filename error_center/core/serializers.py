c5cea1ab9d39b3006bfa1471134275fb6307fa82
Carlos Moreno <omorenodovale@gmail.com>
2020-07-08 00:44:29 -0500

    Adjustments to event serializers

--- error_center/core/serializers.py
+++ error_center/core/serializers.py
@@ -6,11 +6,14 @@ from .models import Agent, Event
 class AgentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Agent
-        fields = ["url", "name", "status", "environment", "version", "address"]
+        fields = ["id", "url", "name", "status", "environment", "version", "address"]
 
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
+    agent_id = serializers.UUIDField()
+    user_id = serializers.UUIDField()
+
     class Meta:
         model = Event
-        fields = ["url", "level", "message", "shelved", "date", "number_of_occurrences",
-                  "agent", "user"]
+        fields = ["id", "url", "level", "message", "shelved", "date", "number_of_occurrences",
+                  "agent_id", "user_id"]
